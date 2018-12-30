ce0cfd6c6e7012b764c3af13f3331c9ba9b64501
Rogach <platon7pronko@gmail.com>
2018-12-30 13:08:52 +0300

    set up argument parsing

--- benches/main.rs
+++ benches/main.rs
@@ -1,14 +1,20 @@
 #[macro_use] extern crate criterion;
 
 use criterion::{Criterion};
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn criterion_benchmark(c: &mut Criterion) {
     assert!(Command::new("/usr/bin/cargo").args(&["build", "--release"]).status().unwrap().success());
     c.bench_function(
         "run on release build",
         |b| b.iter(|| {
-            assert!(Command::new("target/release/mkprompt").status().unwrap().success());
+            assert!(
+                Command::new("target/release/mkprompt")
+                    .args(&["../bitcluster.ru"])
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::null())
+                    .status().unwrap().success()
+            );
         })
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,22 @@
+extern crate clap;
+
+use clap::{App, Arg, AppSettings};
+use std::env;
+use std::path::{PathBuf};
+
 fn main() {
+    let app = App::new("mkprompt")
+        .arg(Arg::with_name("PATH")
+             .help("use path instead of current working dir")
+             .required(false)
+             .index(1))
+        .setting(AppSettings::DisableVersion);
+    let matches = app.get_matches();
+
+    let path = match matches.value_of("PATH") {
+        Some(p) => PathBuf::from(p),
+        None => env::current_dir().unwrap()
+    }.canonicalize().unwrap();
+
+    println!("{:?}", path);
 }
