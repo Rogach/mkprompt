8ccfd3ecec8c78b0868fb16b10a1692df9cbd64a
Rogach <platon7pronko@gmail.com>
2019-01-03 12:38:43 +0300

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::path::{PathBuf};
 use std::fs;
 use std::os::linux::fs::MetadataExt;
 use std::process::{Command};
+use std::process::exit;
+use std::io;
 use git2::{Repository, Branch, StatusOptions, Status};
 
 static COLOR_RED: &str = "\\[\\033[0;31m\\]";
@@ -19,6 +21,16 @@ static COLOR_LIGHT_BLUE: &str = "\\[\\033[1;34m\\]";
 static COLOR_NONE: &str = "\\[\\033[0m\\]";
 static COLOR_HOSTNAME: &str = "\\[\\033[38;5;28m\\]";
 
+static FALLBACK_PROMPT: &str = "\\[\\033[0m\\]\\u@\\h \\w\\n$ ";
+
+fn exit_with_fallback<E>(err: E) -> !
+    where E: std::error::Error  {
+
+    eprintln!("{}", err);
+    println!("{}", FALLBACK_PROMPT);
+    exit(1)
+}
+
 fn main() {
     let app = App::new("mkprompt")
         .arg(Arg::with_name("PATH")
@@ -30,29 +42,36 @@ fn main() {
 
     let path = match matches.value_of("PATH") {
         Some(p) => PathBuf::from(p),
-        None => env::current_dir().unwrap()
-    }.canonicalize().unwrap();
+        None => env::current_dir().unwrap_or_else(|e| exit_with_fallback(e))
+    }.canonicalize().unwrap_or_else(|e| exit_with_fallback(e));
 
-    let root_filesystem_dev = fs::metadata("/").unwrap().st_dev();
-    let path_filesystem_dev = fs::metadata(path.clone()).unwrap().st_dev();
+    let path_str = path.to_str().unwrap_or_else(
+        || exit_with_fallback(io::Error::new(
+            io::ErrorKind::Other,
+            "unable to convert path to string"
+        ))
+    );
+
+    let root_filesystem_dev =
+        fs::metadata("/").unwrap_or_else(|e| exit_with_fallback(e)).st_dev();
+    let path_filesystem_dev =
+        fs::metadata(path.clone()).unwrap_or_else(|e| exit_with_fallback(e)).st_dev();
     let on_root_filesystem = root_filesystem_dev == path_filesystem_dev;
 
     let git_prompt =
         if on_root_filesystem {
-            get_git_prompt(&path)
+            get_git_prompt(&path).unwrap_or_else(|err| {
+                eprintln!("{}", err);
+                String::new()
+            })
         } else {
             "".into()
         };
 
-    let sudo_available =
-        Command::new(format!("{}/bin/checksudo", env::var("HOME").unwrap()))
-        .status()
-        .unwrap();
-
     println!("{}", [
         COLOR_LIGHT_GREEN.into(),
         "\\u".into(),
-        if sudo_available.success() {
+        if is_sudo_available() {
             format!("{}!", COLOR_RED)
         } else {
             format!("{}@", COLOR_HOSTNAME)
@@ -60,7 +79,7 @@ fn main() {
         COLOR_HOSTNAME.into(),
         "\\h ".into(),
         COLOR_LIGHT_BLUE.into(),
-        path.to_str().unwrap().into(),
+        path_str.into(),
         COLOR_NONE.into(),
         " ".into(),
         git_prompt,
@@ -70,16 +89,16 @@ fn main() {
     ].concat());
 }
 
-fn get_git_prompt(path: &PathBuf) -> String {
+fn get_git_prompt(path: &PathBuf) -> Result<String, git2::Error> {
     if let Ok(mut git_repo) = Repository::discover(path) {
 
         let mut stash_count = 0;
         git_repo.stash_foreach(|_idx, _name, _oid| {
             stash_count += 1;
             true
-        }).unwrap();
+        })?;
 
-        let head = git_repo.head().unwrap();
+        let head = git_repo.head()?;
         let branch_opt = if head.is_branch() {
             Some(Branch::wrap(head))
         } else {
@@ -87,7 +106,7 @@ fn get_git_prompt(path: &PathBuf) -> String {
         };
         let branch_name_opt: Option<String> =
             if let Some(branch) = &branch_opt {
-                branch.name().unwrap().map(|s| s.to_owned())
+                branch.name()?.map(|s| s.to_owned())
             } else {
                 None
             };
@@ -98,7 +117,7 @@ fn get_git_prompt(path: &PathBuf) -> String {
                  .include_unmodified(false)
                  .renames_head_to_index(false)
                  .renames_index_to_workdir(false))
-        ).unwrap();
+        )?;
 
         let aggregate_status = statuses.iter().fold(Status::empty(), |acc, se| acc | se.status());
         let has_staged_changes = aggregate_status.intersects(
@@ -121,7 +140,7 @@ fn get_git_prompt(path: &PathBuf) -> String {
                 if let Ok(remote_branch) = branch.upstream() {
                     if let Some(branch_oid) = branch.get().target() {
                         if let Some(remote_branch_oid) = remote_branch.get().target() {
-                            git_repo.graph_ahead_behind(branch_oid, remote_branch_oid).unwrap()
+                            git_repo.graph_ahead_behind(branch_oid, remote_branch_oid)?
                         } else {
                             (0, 0)
                         }
@@ -135,7 +154,7 @@ fn get_git_prompt(path: &PathBuf) -> String {
                 (0, 0)
             };
 
-        [
+        Ok([
             if branch_opt.is_none() {
                 COLOR_RED.into()
             } else if has_unstaged_changes {
@@ -168,8 +187,28 @@ fn get_git_prompt(path: &PathBuf) -> String {
                 "".into()
             },
             COLOR_NONE.into(),
-        ].concat()
+        ].concat())
     } else {
-        "".into()
+        Ok("".into())
+    }
+}
+
+fn is_sudo_available() -> bool {
+    match env::var("HOME") {
+        Err(err) => {
+            eprintln!("{}", err);
+            false
+        },
+        Ok(home) => {
+            match Command::new(format!("{}/bin/checksudo", home)).status() {
+                Err(err) => {
+                    eprintln!("{}", err);
+                    false
+                },
+                Ok(status) => {
+                    status.success()
+                }
+            }
+        }
     }
 }
